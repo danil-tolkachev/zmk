323713dde87fc2b668ecac5e1ed1e1a86b8215a9
Danil Tolkachev <dandur.oak@gmail.com>
2025-04-22 14:25:46 +0300

    disable pinmux

--- app/boards/arm/nrfmicro/pinmux.c
+++ app/boards/arm/nrfmicro/pinmux.c
@@ -11,17 +11,6 @@
 #include <zephyr/sys/sys_io.h>
 #include <zephyr/devicetree.h>
 
-static int pinmux_nrfmicro_init(void) {
-#if (CONFIG_BOARD_NRFMICRO_13 || CONFIG_BOARD_NRFMICRO_13_52833)
-    const struct device *p0 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
-#if CONFIG_BOARD_NRFMICRO_CHARGER
-    gpio_pin_configure(p0, 5, GPIO_OUTPUT);
-    gpio_pin_set(p0, 5, 0);
-#else
-    gpio_pin_configure(p0, 5, GPIO_INPUT);
-#endif
-#endif
-    return 0;
-}
+static int pinmux_nrfmicro_init(void) { return 0; }
 
 SYS_INIT(pinmux_nrfmicro_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
